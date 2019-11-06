41871266cf2a576770c54d461b97aa9e46461f33
Shital <shital31>
2019-11-06 17:11:15 +0530

    practice examples

---
+++ variables.js
@@ -0,0 +1,6 @@
+var num = 5;
+var a = true;
+var c = "Shital";
+var e = [1, 2, 3];
+var f = [1, true, "a"];
+console.log(e);
---
+++ variables.ts
@@ -0,0 +1,6 @@
+let num: number = 5;
+let a: boolean = true;
+let c: string = "Shital";
+let e: number[] = [1, 2, 3];
+let f: any[] = [1, true, "a"];
+console.log(e);
